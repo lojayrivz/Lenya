37adef100d8bdffb5cae0d25ee53a670dbfab5c1
lojayrivz <lojayrivz@gmail.com>
2019-08-29 12:31:30 +0800

    First not so working prototype

---
+++ lib/Screens/Pageone.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Pageone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: ListView(
+        children: <Widget>[
+          Image.asset('lib/Images/MangInasal.jpg'),
+          Image.asset('lib/Images/JolibeeFoods.jpg'),
+          Image.asset('lib/Images/Chowking.jpg')
+        ],
+     ),
+    );
+  }
+}
